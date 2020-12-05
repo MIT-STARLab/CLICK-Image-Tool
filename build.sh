4815b747aec6594d835875191d46dc8effdbb882
Ondrej Cierny <ondrej@mit.edu>
2020-12-05 14:55:55 -0500

    Kernel commit

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+# Main script to build a CLICK golden image
 
+# Initialize
 cores=$(($(nproc)+1))
+if [ ! -d "output" ] && mkdir output
 
-# submodule update
-
-mkdir -p build
+# Run buildroot
 cd buildroot
-make defconfig BR2_DEFCONFIG=../config_buildroot.txt BR2_JLEVEL=$cores O=../build 
+make defconfig BR2_DEFCONFIG=../config_buildroot.txt BR2_EXTERNAL=../extras BR2_JLEVEL=$cores O=../output
+cd ../output
+make BR2_JLEVEL=$cores
