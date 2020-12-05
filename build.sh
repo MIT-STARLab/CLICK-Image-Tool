735e61c38ee2fee1e1108f2397a8ad4f54124e6b
Ondrej Cierny <ondrej@mit.edu>
2020-12-05 12:44:11 -0500

    Script flags

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+cores=$(($(nproc)+1))
+
 # submodule update
 
 mkdir -p build
 cd buildroot
-make defconfig BR2_DEFCONFIG=../config_buildroot.txt O=../build
+make defconfig BR2_DEFCONFIG=../config_buildroot.txt BR2_JLEVEL=$cores O=../build 
