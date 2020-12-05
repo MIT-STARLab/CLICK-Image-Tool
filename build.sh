7fcdc16c9973e28ced9ea041fd200853fca03b6b
Ondrej Cierny <ondrej@mit.edu>
2020-12-05 15:06:01 -0500

    Submodule update

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # Main script to build a CLICK golden image
 
-# Initialize
-cores=$(($(nproc)+1))
+# Initialization
+cores=$(nproc)
 if [ ! -d "output" ] && mkdir output
+git submodule update --recursive --remote --init --jobs $cores
 
 # Run buildroot
 cd buildroot
 make defconfig BR2_DEFCONFIG=../config_buildroot.txt BR2_EXTERNAL=../extras BR2_JLEVEL=$cores O=../output
 cd ../output
-make BR2_JLEVEL=$cores
+# make BR2_JLEVEL=$cores
