6e16852cf9c4046c70b1cb11eaefd561fa7a2006
Ondrej Cierny <ondrej@mit.edu>
2020-12-05 20:55:08 -0500

    Remove usbboot repo

--- post_image.sh
+++ post_image.sh
@@ -3,19 +3,17 @@
 # buildroot/board/raspberrypi3/post-image.sh
 set -e
 
-GENIMAGE_CFG="config_image.txt"
-GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
-
-cp config_rpi.txt "${BINARIES_DIR}/rpi-firmware/config.txt"
+[ -f "${BINARIES_DIR}/zImage" ] && mv "${BINARIES_DIR}/zImage" "${BINARIES_DIR}/kernel"
+[ ! -f "${BINARIES_DIR}/config.txt" ] && cp "../config_rpi.txt" "${BINARIES_DIR}/config.txt"
 
 # Pass an empty rootpath. genimage makes a full copy of the given rootpath to
 # ${GENIMAGE_TMP}/root so passing TARGET_DIR would be a waste of time and disk
 # space. We don't rely on genimage to build the rootfs image, just to insert a
 # pre-built one in the disk image.
-
 trap 'rm -rf "${ROOTPATH_TMP}"' EXIT
 ROOTPATH_TMP="$(mktemp -d)"
 
+GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 rm -rf "${GENIMAGE_TMP}"
 
 genimage \
@@ -23,6 +21,6 @@ genimage \
 	--tmppath "${GENIMAGE_TMP}"    \
 	--inputpath "${BINARIES_DIR}"  \
 	--outputpath "${BINARIES_DIR}" \
-	--config "${GENIMAGE_CFG}"
+	--config "../config_image.txt"
 
 exit $?
