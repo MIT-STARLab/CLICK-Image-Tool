2d49531b21f47341bdd4a4e1e8460393d5e8ac56
Ondrej Cierny <ondrej@mit.edu>
2020-12-05 22:57:29 -0500

    Symlink to root

--- build.sh
+++ build.sh
@@ -5,9 +5,10 @@
 cores=$(nproc)
 git submodule update --remote --rebase --init --jobs $cores
 [ ! -d "output" ] && mkdir output
+[ ! -L "overlay/root" ] && ln -s "$(pwd)/click" overlay/root
 
 # Run buildroot
-cd buildroot
-make defconfig BR2_DEFCONFIG=../config_buildroot.txt BR2_EXTERNAL=../extra BR2_JLEVEL=$cores O=../output
-cd ../output
-make BR2_JLEVEL=$cores
+# cd buildroot
+# make defconfig BR2_DEFCONFIG=../config_buildroot.txt BR2_EXTERNAL=../extra BR2_JLEVEL=$cores O=../output
+# cd ../output
+# make BR2_JLEVEL=$cores
