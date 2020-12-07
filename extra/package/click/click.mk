346d9747bc00df5de0c18f12054446ca20f97ae6
Ondrej Cierny <ondrej@mit.edu>
2020-12-07 16:32:46 -0500

    New kernel config

--- extra/package/click/click.mk
+++ extra/package/click/click.mk
@@ -4,15 +4,12 @@ CLICK_SITE = $(call github,MIT-STARLab,CLICK-A-RPi,master)
 CLICK_LICENSE = LGPL-3.0
 CLICK_LICENSE_FILES = LICENSE.md
 CLICK_MODULE_SUBDIRS = bus/driver
+CLICK_LINUX_CONFIG_FIXUPS = $(call KCONFIG_SET_OPT,CONFIG_UNUSED_KSYMS_WHITELIST,$(BR2_EXTERNAL_CLICK_PATH)/package/click/ksym.txt)
 
-# The kernel driver needs kfifo symbols, which no other modules use...
-# To include the symbols, force the kernel to build a kfifo sample module
-CLICK_LINUX_CONFIG_FIXUPS = $(call KCONFIG_ENABLE_OPT,CONFIG_SAMPLES) $(call KCONFIG_SET_OPT,CONFIG_SAMPLE_KFIFO,m)
+$(eval $(kernel-module))
+$(eval $(generic-package))
 
 # Command to install the flight software to /root
 define CLICK_INSTALL_TARGET_CMDS
 	rsync -a --exclude='.*' --exclude='*.md' --exclude='*~' --exclude='*.o'} $(@D)/ $(TARGET_DIR)/root/
 endef
-
-$(eval $(kernel-module))
-$(eval $(generic-package))
