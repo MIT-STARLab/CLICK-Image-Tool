c1d3d2f9c26733a414b1bcc61970eed89f49d1a1
Ondrej Cierny <ondrej@mit.edu>
2020-12-07 18:33:42 -0500

    Kernel flag fixes

--- extra/package/click/click.mk
+++ extra/package/click/click.mk
@@ -1,10 +1,10 @@
 # Defines CLICK flight software package via a git tag
-CLICK_VERSION = v0.1
+CLICK_VERSION = 69dedc82a2dbff2298b2f38559d5d77050ebb666
 CLICK_SITE = $(call github,MIT-STARLab,CLICK-A-RPi,master)
 CLICK_LICENSE = LGPL-3.0
 CLICK_LICENSE_FILES = LICENSE.md
 CLICK_MODULE_SUBDIRS = bus/driver
-CLICK_LINUX_CONFIG_FIXUPS = $(call KCONFIG_SET_OPT,CONFIG_UNUSED_KSYMS_WHITELIST,$(BR2_EXTERNAL_CLICK_PATH)/package/click/ksym.txt)
+CLICK_LINUX_CONFIG_FIXUPS = $(call KCONFIG_SET_OPT,CONFIG_UNUSED_KSYMS_WHITELIST,"$(BR2_EXTERNAL_CLICK_PATH)/package/click/ksym.txt")
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
--- post_build.sh
+++ post_build.sh
@@ -4,29 +4,17 @@
 set -u
 set -e
 
-# Move some system files to tmpfs
-rm -rf ${TARGET_DIR}/run/dbus
-rm -rf ${TARGET_DIR}/var/log/journal
-ln -sfn /tmp ${TARGET_DIR}/var/spool
-ln -sfn /tmp ${TARGET_DIR}/var/lock
-ln -sfn /tmp ${TARGET_DIR}/var/tmp
-
-# Configure the filesystems
-if [ -e ${TARGET_DIR}/etc/fstab ] && ! grep -qE '^tmpfs' ${TARGET_DIR}/etc/fstab; then
-	# echo "/dev/mmcblk0p3 /root ext4 defaults 0" >> ${TARGET_DIR}/etc/fstab
-    # echo "tmpfs /tmp tmpfs nosuid,nodev 0 0" >> ${TARGET_DIR}/etc/fstab
-    echo "tmpfs /run tmpfs nosuid,nodev 0 0" >> ${TARGET_DIR}/etc/fstab
-    echo "tmpfs /var/log tmpfs nosuid,nodev 0 0" >> ${TARGET_DIR}/etc/fstab
-fi
-
 # Disable some systemd default services
 ln -sfn /dev/null ${TARGET_DIR}/etc/systemd/system/systemd-timesyncd.service
+ln -sfn /dev/null ${TARGET_DIR}/etc/systemd/system/systemd-resolved.service
 ln -sfn /dev/null ${TARGET_DIR}/etc/systemd/system/getty@.service
 ln -sfn /dev/null ${TARGET_DIR}/etc/systemd/system/remote-fs.target
+ln -sfn /dev/null ${TARGET_DIR}/etc/systemd/system/systemd-remount-fs.service
 ln -sfn /dev/null ${TARGET_DIR}/etc/systemd/system/sys-fs-fuse-connections.mount
-ln -sfn /dev/null ${TARGET_DIR}/etc/systemd/system/systemd-ask-password-console.path 
+ln -sfn /dev/null ${TARGET_DIR}/etc/systemd/system/systemd-ask-password-console.service 
 ln -sfn /dev/null ${TARGET_DIR}/etc/systemd/system/systemd-machine-id-commit.service
 ln -sfn /dev/null ${TARGET_DIR}/etc/systemd/system/systemd-hwdb-update.service
+ln -sfn /dev/null ${TARGET_DIR}/etc/systemd/system/systemd-tmpfiles-setup.service
 
 # Auto login root if a UART tty is running
 cp ${TARGET_DIR}/usr/lib/systemd/system/serial-getty@.service \
