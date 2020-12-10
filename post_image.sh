8e0827b6a52e685087f22ba3ceae79903bdbeccd
Ondrej Cierny <ondrej@mit.edu>
2020-12-09 22:21:27 -0500

    Working filesystems

--- post_image.sh
+++ post_image.sh
@@ -15,15 +15,23 @@ if [ ! -d "${BINARIES_DIR}/overlays" ] ; then
 		"${TARGET_DIR}/usr/local/fsw/bus/driver/click_spi.dts"
 fi
 
-# Calculate partition sizes
+# Constants to help calculate partition sizes
 RPI_FLASH_SECTOR_SIZE=512
 RPI_FLASH_TOTAL_SECTORS=7634943
-RPI_BOOTPART_OVERHEAD=$((86*RPI_FLASH_SECTOR_SIZE))
+BOOT_PART_OVERHEAD=$((86*RPI_FLASH_SECTOR_SIZE))
+EXT4_HEADER_SIZE=5120
 
+# The actual large R/W ext4 partition is created by the OS on first boot
+# Here, we only write a small empty block to make sure the ext4 header is empty after reflashing 
+# Otherwise the ext4 auto-mount might freak out if the header is malformed on first boot
+dd if=/dev/zero of=${BINARIES_DIR}/rw_zeros.ext4 seek=$EXT4_HEADER_SIZE count=0 bs=1 2>/dev/null
+
+# Function to align a value to flash sector size
 function align_to_flash {
 	SIZE_ALIGNED=$(($1+RPI_FLASH_SECTOR_SIZE-1-(($1+RPI_FLASH_SECTOR_SIZE-1)%RPI_FLASH_SECTOR_SIZE))) 
 }
 
+# Files used to calculate boot partition size
 declare -a bootfiles=(
     "overlays/click_spi.dtbo"
     "bcm2710-rpi-cm3.dtb"
@@ -34,31 +42,34 @@ declare -a bootfiles=(
     "rpi-firmware/fixup.dat"
     "rpi-firmware/start.elf")
 
+# Calculate boot partition size
 for f in "${bootfiles[@]}"; do
     align_to_flash $(stat --printf="%s" "${BINARIES_DIR}/$f")
-    RPI_BOOTPART_OVERHEAD=$((RPI_BOOTPART_OVERHEAD+SIZE_ALIGNED))
+    BOOT_PART_OVERHEAD=$((BOOT_PART_OVERHEAD+SIZE_ALIGNED))
 done
 
+# Calculate OS partition size
 align_to_flash $(stat --printf="%s" "${BINARIES_DIR}/rootfs.squashfs")
-RPI_ROOTPART_SIZE=$SIZE_ALIGNED
+OS_PART_SIZE=$SIZE_ALIGNED
 
-# Calculate free space for FSW partition
-RPI_FSWPART_SIZE=$(((RPI_FLASH_TOTAL_SECTORS*RPI_FLASH_SECTOR_SIZE)-RPI_BOOTPART_OVERHEAD-RPI_ROOTPART_SIZE))
+# Calculate available space for the R/W ext4 partition
+RW_PART_SIZE=$(((RPI_FLASH_TOTAL_SECTORS*RPI_FLASH_SECTOR_SIZE)-BOOT_PART_OVERHEAD-OS_PART_SIZE))
 
-# Prepare config for genimage tool
+# Prepare config for the genimage tool
 bootfiles[0]="overlays"
 bootfiles=$(printf "\"%s\"," "${bootfiles[@]}")
 bootfiles=${bootfiles%?}
 cat > "${BUILD_DIR}/genimage.cfg" <<EOF
-image boot.vfat { vfat { extraargs = "-F 16 -s 1" files = { $bootfiles } } size = $RPI_BOOTPART_OVERHEAD }
+image boot.vfat { vfat { extraargs = "-F 16 -s 1" files = { $bootfiles } } size = $BOOT_PART_OVERHEAD }
 image sdcard.img {
 	hdimage {}
 	partition boot { partition-type = 0xE bootable = "true" image = "boot.vfat" }
-	partition root { partition-type = 0x83 image = "rootfs.squashfs" }
-	partition fsw { partition-type = 0x83 size = $RPI_FSWPART_SIZE }
+	partition os { partition-type = 0x83 image = "rootfs.squashfs" }
+	partition rw { partition-type = 0x83 size = $RW_PART_SIZE image = "rw_zeros.ext4" }
 }
 EOF
 
+# All below copied from buildroot/board/raspberrypi3/post-image.sh
 # Pass an empty rootpath. genimage makes a full copy of the given rootpath to
 # ${GENIMAGE_TMP}/root so passing TARGET_DIR would be a waste of time and disk
 # space. We don't rely on genimage to build the rootfs image, just to insert a
