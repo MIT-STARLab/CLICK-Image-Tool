5c533a8e681d7ca8cfffdf3980e9e96b808fa0fa
Ondrej Cierny <ondrej@mit.edu>
2020-12-17 18:36:33 -0500

    Fix typo

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ export FPGA_VERSION="f9d2ab603b26612674a6ff188968d62685d2ec8f"
 export BOOT_WITH_PPP=1
 
 # Initialization
+SECONDS=0
 cores=$(nproc)
 cores=$((cores+1))
 [ ! -d "img" ] && mkdir img
@@ -16,9 +17,9 @@ cores=$((cores+1))
 [ ! -f "buildroot/Makefile" ] && git submodule update --init --jobs $cores
 
 # Run buildroot
-SECONDS=0
 cd buildroot
 make defconfig BR2_DEFCONFIG=../config_buildroot.txt BR2_EXTERNAL=../extra BR2_JLEVEL=$cores O=../output
 cd ../output
 make BR2_JLEVEL=$cores $@ all
+
 echo "Finished in $((SECONDS/3600))h $(((SECONDS/60)%60))m $((SECONDS%60))s"
