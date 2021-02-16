fc6605b179bdf233ac543fc71c808e655ae4db79
Ondrej Cierny <ondrej@mit.edu>
2021-02-16 13:50:54 -0500

    Camera switch on on boot

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # Main script to build a CLICK golden image
 
 # FSW/FPGA version to use: either a tag or commit hash from CLICK-A-RPi and CLICK-A-FPGA repositories
-export FSW_VERSION="4f540fc6c458daed78bf2dece1aadfe838c1c015"
+export FSW_VERSION="b8419b31ac860cfdb04778d9a0ff0291bc8a63ce"
 export FPGA_VERSION="52d9b65e22dfc850b2855403780c782726e2167d"
 export FPGA_DEFAULT="images/BIST_10.xsvf"
 
